47bc3e21199a20a1e8e454924f98f666ba0754a8
Ananya Jain <mkjainananya27@gmail.com>
2026-02-14 21:55:59 +0530

    frontend changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import HomePage from "./landing_page/home/HomePage.jsx";
+import Signup from "./landing_page/signup/Signup.jsx";
+import AboutPage from "./landing_page/about/AboutPage.jsx";
+import PricingPage from "./landing_page/pricing/PricingPage.jsx";
+import ProductPage from "./landing_page/product/ProductPage.jsx";
+import SupportPage from "./landing_page/support/SupportPage.jsx";
+
+import Navbar from "./landing_page/Navbar.jsx";
+import Footer from "./landing_page/Footer.jsx";
+import PageNotFound from "./landing_page/PageNotFound.jsx";
+
+createRoot(document.getElementById("root")).render(
+  // <StrictMode>
+  //   <HomePage/>
+  // </StrictMode>,
+
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/products" element={<ProductPage />} />
+      <Route path="/pricing" element={<PricingPage />} />
+      <Route path="/support" element={<SupportPage />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+    <Footer />
+  </BrowserRouter>
+);
