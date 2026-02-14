159bdf0ad06e28b633435798cfda3561dd1e7286
Ananya Jain <mkjainananya27@gmail.com>
2026-02-15 00:03:33 +0530

    dashboard setup

---
+++ dashboard/src/components/WatchList.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import { watchlist } from "../data/data";
+import GeneralContext from "./GeneralContext";
+import { Tooltip, Grow } from "@mui/material";
+import {
+  KeyboardArrowDown,
+  KeyboardArrowUp,
+  BarChartOutlined,
+  MoreHoriz,
+} from "@mui/icons-material";
+
+
+const WatchList = () => {
+  return (
+    <div className="watchlist-container">
+      <div className="search-container">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
+          className="search"
+        />
+        <span className="counts"> {watchlist.length} / 50</span>
+      </div>
+
+      <ul className="list">
+        {watchlist.map((stock, index) => {
+          return <WatchListItem stock={stock} key={index} />;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default WatchList;
+
+// WatchList Item component
+
+const WatchListItem = ({ stock }) => {
+  const [showWatchlistActions, setShowWatchlistActions] = useState(false);
+
+  const handleMouseEnter = (e) => {
+    setShowWatchlistActions(true);
+  };
+
+  const handleMouseLeave = (e) => {
+    setShowWatchlistActions(false);
+  };
+
+  return (
+    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div className="item">
+        <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+        <div className="itemInfo">
+          <span className="percent">{stock.percent}</span>
+          {stock.isDown ? (
+            <KeyboardArrowDown className="down" />
+          ) : (
+            <KeyboardArrowUp className="up" />
+          )}
+          <span className="price">{stock.price}</span>
+        </div>
+      </div>
+      {showWatchlistActions && <WatchListActions uid={stock.name} />}
+    </li>
+  );
+};
+
+
+// WatchList Action component
+
+const WatchListActions = ({ uid }) => {
+  //   const generalContext = useContext(GeneralContext);
+
+  //   const handleBuyClick = () => {
+  //     generalContext.openBuyWindow(uid);
+  //   };
+
+  return (
+    <span className="actions">
+      <span>
+        <Tooltip
+          title="Buy (B)"
+          placement="top"
+          arrow
+          //   onClick={handleBuyClick}
+        >
+          <button className="buy ">Buy</button>
+        </Tooltip>
+
+        <Tooltip title="Sell (S)" placement="top" arrow>
+          <button className="sell">Sell</button>
+        </Tooltip>
+
+        <Tooltip title="Analytics (A)" placement="top" arrow>
+          <button className="action">
+            <BarChartOutlined className="icon" />
+          </button>
+        </Tooltip>
+
+        <Tooltip title="More" placement="top" arrow>
+          <button className="action">
+            <MoreHoriz className="icon" />
+          </button>
+        </Tooltip>
+      </span>
+    </span>
+  );
+};
