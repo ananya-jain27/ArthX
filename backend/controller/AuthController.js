92a4bbf2e8e48ce2c1199b4c67ecdfc562505fd2
ananya-jain27 <mkjainananya27@gmail.com>
2026-02-22 13:25:40 +0530

    Set cookie to httpOnly and secure
    Updated cookie settings for improved security.

--- backend/controller/AuthController.js
+++ backend/controller/AuthController.js
@@ -46,7 +46,9 @@ module.exports.Login = async (req, res, next) => {
      const token = createSecretToken(user._id);
      res.cookie("token", token, {
        withCredentials: true,
-       httpOnly: false,
+       httpOnly: true,
+       secure: true,
+       sameSite: "None",
      });
      res.status(201).json({ message: "User logged in successfully", success: true });
      next();
