eed5b4ad5d7572bf99b4ebe87ba0c5099e7b4ebb
ananya-jain27 <mkjainananya27@gmail.com>
2026-04-17 19:48:09 +0530

    Change navbar alignment from left to right

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -20,7 +20,7 @@ function Navbar() {
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse order-lg-1" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
               <Link class="nav-link active" aria-current="page" to="/signup">
                 Signup
