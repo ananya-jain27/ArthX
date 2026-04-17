ada78184a4ef88e43d80078b07617b2aa0f45feb
ananya-jain27 <mkjainananya27@gmail.com>
2026-04-17 19:39:43 +0530

    Update company founding date and name in Hero.jsx

--- frontend/src/landing_page/about/Hero.jsx
+++ frontend/src/landing_page/about/Hero.jsx
@@ -15,26 +15,19 @@ function Hero() {
       <div className="row border-top border-1 mt-5 " style={{fontSize : "16.5px", lineHeight: "1.8em"}}>
         <div className="col p-5">
           <p className="text-muted">
-            We kick-started operations on the 15th of August, 2010 with the goal
+            We kick-started operations on the 15th of August, 2025 with the goal
             of breaking all barriers that traders and investors face in India in
             terms of cost, support, and technology. We named the company
-            Zerodha, a combination of Zero and "Rodha", the Sanskrit word for
-            barrier.
+            ArthX, a combination of Arth and "X", the Sanskrit word for
+            Arth meaning wealth.
           </p>{" "}
           <p className="text-muted">
             Today, our disruptive pricing models and in-house technology have
             made us the biggest stock broker in India.
           </p>{" "}
-          <p className="text-muted">
-            Over 1.6+ crore clients place billions of orders every year through
-            our powerful ecosystem of investment platforms, contributing over
-            15% of all Indian retail trading volumes.
-          </p>
+          
         </div>
         <div className="col p-5">
-          <p className="text-muted">
-            In addition, we run a number of popular open online educational and
-            community initiatives to empower retail traders and investors.</p>
             <p className="text-muted">Rainmatter, our fintech fund and incubator, has invested in several
             fintech startups with the goal of growing the Indian capital
             markets. </p>
