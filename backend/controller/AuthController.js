ba1be35530b4571459c5da1589d2dba784b0459a
ananya-jain27 <mkjainananya27@gmail.com>
2026-05-13 13:26:53 +0530

    Set httpOnly flag to true for authentication cookie

--- backend/controller/AuthController.js
+++ backend/controller/AuthController.js
@@ -16,7 +16,7 @@ module.exports.Signup = async (req, res, next) => {
     const token = createSecretToken(user._id);
     res.cookie("token", token, {
       withCredentials: true,
-      httpOnly: false,
+      httpOnly: true,
     });
     res
       .status(201)
